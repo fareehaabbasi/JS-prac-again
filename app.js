867b3d21414e3700d05ae1397e8a4eab9c0d13c2
fareehaabbasi <abbasifareeha9@gmail.com>
2025-10-17 22:55:44 +0500

    custom-fileinput

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 //approch 
 
-let h1 = document.querySelector("h1");
+// let h1 = document.querySelector("h1");
 
-window.addEventListener("keydown", (dets) => {
-    if(dets.key === " "){
-        h1.textContent = "Space key Pressed";
-    }else {
-        h1.textContent = dets.key;
+// window.addEventListener("keydown", (dets) => {
+//     if(dets.key === " "){
+//         h1.textContent = "Space key Pressed";
+//     }else {
+//         h1.textContent = dets.key;
+//     }
+// })
+
+//approch 2
+
+let btn = document.querySelector("#btn");
+let file = document.querySelector('#fileinp');
+
+btn.addEventListener('click', function() {
+    file.click();
+})
+
+file.addEventListener('change', function(dets) {
+    let filenme = dets?.target.files[0];
+    if(filenme){
+        btn.textContent = filenme.name;
     }
 })
